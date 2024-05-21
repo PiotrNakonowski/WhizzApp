97db2691d859ff9f65f913d48a25621468a12c4a
Adam Plewa <245901@edu.p.lodz.pl>
2024-05-21 22:01:25 +0200

    8. Utworzenie menu nawigacyjnego strony głównej

--- WebApp/src/App.js
+++ WebApp/src/App.js
@@ -1,13 +1,16 @@
 import Login from "./pages/login/Login";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/home/Home";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
   
   return (
       <BrowserRouter>
         <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="login" element={<Login />} />
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+        </Route>
         </Routes>
       </BrowserRouter>
   );
