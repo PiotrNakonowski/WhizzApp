fb90b800d0590ac8b4a78d46332d2d20d327c284
Jakub-708 <240708@edu.p.lodz.pl>
2024-06-02 21:40:59 +0200

    Dodanie fragmetów kodu odpowiedzialnych za funkcjonalność mapy

--- MobileApp/app/src/main/java/com/example/whizzapp/Homepage.java
+++ MobileApp/app/src/main/java/com/example/whizzapp/Homepage.java
@@ -138,7 +138,7 @@ public class Homepage extends AppCompatActivity {
             }
         });
 
-       /* mapButton.setOnClickListener(new View.OnClickListener() {
+        mapButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (currentActivityClass != NavigationMap.class) {
@@ -150,7 +150,8 @@ public class Homepage extends AppCompatActivity {
                     drawerLayout.closeDrawer(GravityCompat.START);
                 }
             }
-        });*/
+        });
+
 
        /* todoButton.setOnClickListener(new View.OnClickListener() {
             @Override
